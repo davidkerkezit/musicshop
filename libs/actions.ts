d76700945464b83c0e383fa28a54b2ea701f1436
davidkerkezit <davidkerkez@gmail.com>
2024-02-17 16:57:35 +0100

    fix

--- libs/actions.ts
+++ libs/actions.ts
@@ -391,8 +391,8 @@ export async function editProduct(formData: any) {
       const uploadedProductData = await res.json();
       // Handle success scenario
 
-return uploadedProductData
-
+      return uploadedProductData;
+      // add test comment
       // You can perform additional actions here like refreshing or navigating to another page
     } else if (
       category !== formData.currentCategory &&
