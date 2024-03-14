c8523b5ead53c70686a3eb44796747a1d7183b5a
davidkerkezit <davidkerkez@gmail.com>
2024-03-14 20:29:52 +0100

    dashboard products grid fix

--- components/Shop/Products.tsx
+++ components/Shop/Products.tsx
@@ -18,7 +18,7 @@ const Products = ({
       <div
         className={` grid   gap-2 ${
           parent === "dashboard"
-            ? " grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+            ? " grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
             : "lg:grid-cols-4 grid-cols-2 sm:grid-cols-3"
         }`}
       >
