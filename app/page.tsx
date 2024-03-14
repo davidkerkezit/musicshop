b2745e2a4b2f44fdf6ea3793971c6efa9f1e6f8d
davidkerkezit <davidkerkez@gmail.com>
2024-03-14 17:54:02 +0100

    openGraph

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,19 @@ import {
   Categories,
   Testimonials,
 } from "@/components/Home";
+import LOGO from "@/assets/logo.png";
+
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Music Shop | Home",
   description:
     "Welcome to Musicshop London - your premier destination for vinyl records, DJ equipment, and audio software. Explore our extensive collection and elevate your music experience today.",
+  openGraph: {
+    images:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQDGQ55_znshhGDlxv5sDZz96tD3hxPc5j8CVWMKvJcw&s",
+  },
 };
+
 export default function Home() {
   return (
     <main>
